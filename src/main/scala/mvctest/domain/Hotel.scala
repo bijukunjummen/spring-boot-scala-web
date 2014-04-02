7128a8f0fd3ff444795d7d275cf7ddf2e8a7a19c
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2014-04-01 22:41:24 -0400

    Added flows for create/edit/delete

--- src/main/scala/mvctest/domain/Hotel.scala
+++ src/main/scala/mvctest/domain/Hotel.scala
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue
 import java.lang.Long
 import javax.persistence.Entity
 import scala.beans.BeanProperty
+import org.hibernate.validator.constraints.NotEmpty
 
 @Entity
 class Hotel {
@@ -15,11 +16,14 @@ class Hotel {
   var id: Long = _
   
   @BeanProperty
+  @NotEmpty
   var name: String = _
   
   @BeanProperty
+  @NotEmpty
   var address: String = _
   
   @BeanProperty
+  @NotEmpty
   var zip: String = _
 }
--- src/main/scala/mvctest/web/HotelController.scala
+++ src/main/scala/mvctest/web/HotelController.scala
@@ -5,15 +5,59 @@ import org.springframework.stereotype.Controller
 import mvctest.service.HotelRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.ui.Model
+import java.lang.Long
+import org.springframework.web.bind.annotation.PathVariable
+import mvctest.domain.Hotel
+import org.springframework.validation.BindingResult
+import org.springframework.web.bind.annotation.RequestMethod
+import javax.validation.Valid
 
 @Controller
 @RequestMapping(Array("/hotels"))
 class HotelController @Autowired() (private val hotelRepository: HotelRepository) {
 	
-  @RequestMapping(Array("/list"))
+  @RequestMapping(method=Array(RequestMethod.GET))
   def list(model: Model) = {
 	val hotels = hotelRepository.findAll()
 	model.addAttribute("hotels", hotels)
 	"hotels/list"
+  }
+  
+  @RequestMapping(Array("/edit/{id}"))
+  def edit(@PathVariable("id") id: Long, model: Model) = {
+    model.addAttribute("hotel", hotelRepository.findOne(id))
+    "hotels/edit"
+  }
+
+  @RequestMapping(method=Array(RequestMethod.GET), params=Array("form"))
+  def createForm(model: Model) = {
+    model.addAttribute("hotel", new Hotel())
+    "hotels/create"
+  }
+  
+  @RequestMapping(method=Array(RequestMethod.POST))
+  def create(@Valid hotel: Hotel, bindingResult: BindingResult) = {
+    if (bindingResult.hasErrors()) {
+      "hotels/create"
+    }else {
+      hotelRepository.save(hotel)
+      "redirect:/hotels"
+    }
+  }
+
+  @RequestMapping(value=Array("/update"), method=Array(RequestMethod.POST))
+  def update(@Valid hotel: Hotel, bindingResult: BindingResult) = {
+    if (bindingResult.hasErrors()) {
+      "hotels/edit"
+    }else {
+      hotelRepository.save(hotel)
+      "redirect:/hotels"
+    }
+  }
+  
+  @RequestMapping(value=Array("/delete/{id}"))
+  def delete(@PathVariable("id") id: Long) = {
+    hotelRepository.delete(id)
+    "redirect:/hotels"
   }
 }
