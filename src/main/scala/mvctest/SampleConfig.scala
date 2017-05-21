2d711ba116af21c447ee072c1841bdc4ad145aa3
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-05-20 21:37:26 -0700

    Upgraded version of scala to 2.12.2

--- src/main/scala/mvctest/SampleConfig.scala
+++ src/main/scala/mvctest/SampleConfig.scala
@@ -1,10 +1,6 @@
 package mvctest
 
-import org.springframework.context.annotation.Configuration
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.context.annotation.ComponentScan
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 class SampleConfig
